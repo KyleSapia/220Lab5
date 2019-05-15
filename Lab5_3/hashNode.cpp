a90989c492ede1b1549112bd39c7e83fb140834e
kylesapia <kylesapia@wifi-roaming-128-4-94-190.host.udel.edu>
2019-05-15 17:54:46 -0400

    Initial commit

---
+++ Lab5_3/hashNode.cpp
@@ -0,0 +1,56 @@
+/*
+ * hashNode.cpp
+ *
+ *  Created on: May 9, 2019
+ *      Author: kylesapia
+ */
+
+#include "hashNode.hpp"
+#include <time.h>
+hashNode::hashNode(){
+    valuesSize=0;
+    currSize=0;
+    values=NULL;
+    keyword="";
+}
+hashNode::hashNode(string s){
+    keyword=s;
+    valuesSize=100;
+    currSize=0;
+    values=new string[valuesSize];
+}
+hashNode::hashNode(string s, string v){
+    keyword=s;
+    valuesSize=100;
+    currSize=1;
+    values=new string[valuesSize];
+    values[0]=v;
+}
+void hashNode:: addValue(string v){
+    int counter=0;
+    while(values[counter]!= ""){
+        counter++;
+    }
+    values[counter]=v;
+    double loadFactor=currSize/(double)valuesSize; //this makes sure it becomes a double
+    if (loadFactor>=0.7){ //load factor goes above 70%
+        dblArray(); //function which doulbes the array
+    }
+}
+void hashNode::dblArray(){
+    string *doubleValues=new string[2*valuesSize];
+    for (int i=0;i<valuesSize;i++){
+        values[i]=doubleValues[i];
+    }
+    valuesSize*=2;
+    values=doubleValues;
+    delete doubleValues;
+}
+string hashNode:: getRandValue(){
+    srand(time(NULL));
+    int random= rand() % valuesSize;
+    if (values[random]== ""){
+        return"";
+    }
+    return values[random];
+}
---
+++ Lab5_3/mainLab5.cpp
@@ -0,0 +1,20 @@
+/*
+ * mainLab5.cpp
+ *
+ *  Created on: May 15, 2019
+ *      Author: kylesapia
+ */
+
+
+#include <iostream>
+using namespace std;
+#include <time.h>
+#include "hashMap.hpp"
+#include "hashNode.hpp"
+#include "makeSeuss.hpp"
+
+int main() {
+	cout << "Hi" << endl;
+	makeSeuss("DrSeuss.txt", "NewText.txt", true, true);
+	return 0;
+}
